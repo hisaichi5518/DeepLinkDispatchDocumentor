c1cefa4555908a89db8c09f4e4794c4777edc896
hisaichi5518 <hisaichi5518@gmail.com>
2016-09-15 15:46:03 +0900

    Added sample deeplink

--- sample/src/main/java/com/github/hisaichi5518/deeplinkdispatchdocumentor/sample/MainActivity.java
+++ sample/src/main/java/com/github/hisaichi5518/deeplinkdispatchdocumentor/sample/MainActivity.java
@@ -3,6 +3,9 @@ package com.github.hisaichi5518.deeplinkdispatchdocumentor.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.deeplinkdispatch.DeepLink;
+
+@DeepLink({"yuriko-app://yoshitaka/yuriko/{id}", "yuriko-app://yositaka/yuriko/{id}"})
 public class MainActivity extends AppCompatActivity {
 
     @Override
