b91ea73293b40365155888af66fb1dd52b40a617
hisaichi5518 <hisaichi5518@gmail.com>
2016-09-15 15:35:59 +0900

    Added Documentor#write

--- processor/src/main/java/com/github/hisaichi5518/deeplinkdispatchdocumentor/model/Documentor.java
+++ processor/src/main/java/com/github/hisaichi5518/deeplinkdispatchdocumentor/model/Documentor.java
@@ -1,4 +1,28 @@
 package com.github.hisaichi5518.deeplinkdispatchdocumentor.model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Documentor {
+    private final Logger logger;
+
+    public Documentor(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void write(File file, String document) throws IOException {
+        File parent = file.getParentFile();
+
+        if (parent != null) {
+            boolean isCreated = parent.mkdirs();
+            if (isCreated) {
+                logger.note("Created parent directory.");
+            }
+        }
+
+        FileWriter writer = new FileWriter(file);
+        writer.write(document);
+        writer.close();
+    }
 }
